317a9462cecc32b0c34c7429ca4045b567bd3d6a
Philipp Winkler <winkler@webit.de>
2022-08-10 15:41:02 +0200

    [FEATURE] init commit

---
+++ lib/shirt_api_map_and_creator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "shirt_api_map_and_creator/version"
+
+module ShirtApiMapAndCreator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/shirt_api_map_and_creator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ShirtApiMapAndCreator do
+  it "has a version number" do
+    expect(ShirtApiMapAndCreator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
